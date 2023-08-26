792370a0260eed2aad8419b7ef8a62b9d96f159a
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-08-26 10:06:06 +0100

    feat: add shop page

---
+++ src/stores/cart.ts
@@ -0,0 +1,24 @@
+import { computed, map } from 'nanostores';
+import type { CartItem, ShopItem } from '../types';
+
+export const $cart = map<Record<number, CartItem>>({});
+
+export const addItemToCart = (item: ShopItem) => {
+  const cartItem = $cart.get()[item.id];
+  const quantity = cartItem ? cartItem.quantity : 0;
+  $cart.setKey(item.id, {
+    item,
+    quantity: quantity + 1,
+  });
+}
+
+export const removeItemFromCart = (itemId: number) => {
+  // @ts-ignore
+  $cart.setKey(itemId, undefined);
+}
+
+export const $subtotal = computed($cart, (cart) => {
+  return Object.values(cart).reduce((acc, cartItem) => {
+    return acc + (cartItem.item.price * cartItem.quantity);
+  }, 0);
+});
