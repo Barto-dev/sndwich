d7d3f0a67e15dcdc329229ea5135f1ee1192729d
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-08-26 10:25:10 +0100

    feat: add cart component

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,12 +1,12 @@
 import { computed, map } from 'nanostores';
 import type { CartItem, ShopItem } from '../types';
 
-export const $cart = map<Record<number, CartItem>>({});
+export const cart = map<Record<number, CartItem>>({});
 
 export const addItemToCart = (item: ShopItem) => {
-  const cartItem = $cart.get()[item.id];
+  const cartItem = cart.get()[item.id];
   const quantity = cartItem ? cartItem.quantity : 0;
-  $cart.setKey(item.id, {
+  cart.setKey(item.id, {
     item,
     quantity: quantity + 1,
   });
@@ -14,10 +14,10 @@ export const addItemToCart = (item: ShopItem) => {
 
 export const removeItemFromCart = (itemId: number) => {
   // @ts-ignore
-  $cart.setKey(itemId, undefined);
+  cart.setKey(itemId, undefined);
 }
 
-export const $subtotal = computed($cart, (cart) => {
+export const subtotal = computed(cart, (cart) => {
   return Object.values(cart).reduce((acc, cartItem) => {
     return acc + (cartItem.item.price * cartItem.quantity);
   }, 0);
